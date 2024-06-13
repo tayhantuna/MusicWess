8461c7f809da7b40f67b0f78ea51728f7bff9aef
tayhantuna <158285886+tayhantuna@users.noreply.github.com>
2024-06-13 15:14:30 +0300

    Update firebaseInit.js

--- firebaseInit.js
+++ firebaseInit.js
@@ -1,13 +1,1 @@
-// firebaseInit.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-import { getFirestore, collection, query, where, getDocs, addDoc, updateDoc, doc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
-import { firebaseConfig } from './firebaseConfig.js';
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const firestore = getFirestore(app);
-const storage = getStorage(app);
-
-export { auth, firestore, storage, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, collection, query, where, getDocs, addDoc, updateDoc, doc, ref, uploadBytes, getDownloadURL };
